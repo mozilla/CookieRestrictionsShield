3ffffeb75d028779cef14638068bada8cc55671c
Erica Wright <ericaewright@gmail.com>
2018-08-30 16:34:48 -0400

    Record tracking protection exception state.

--- src/tabs.js
+++ src/tabs.js
@@ -37,6 +37,7 @@ window.TabRecords = {
       "privacy_trackingprotection_enabled": false,
       "login_form_on_page": false,
       "embedded_social_login_script": false,
+      "user_has_tracking_protection_exception": false,
     };
 
     return tabInfo;
